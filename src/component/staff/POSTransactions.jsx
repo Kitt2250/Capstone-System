0d080320920488d2606b4ecbd483a315251a0976
Kitt2250 <delapenamj603@gmail.com>
2026-09-04 15:17:30 +0800

    fix: set all 4 Grave Lots and Cart table columns to equal 25% width each

--- src/component/staff/POSTransactions.jsx
+++ src/component/staff/POSTransactions.jsx
@@ -1092,8 +1092,8 @@ export default function POSTransactions() {
                     <table class="product-table" id="productTable">
                         <thead>
                             <tr>
-                                <th style="width:30%;">Product</th>
-                                <th style="width:20%;text-align:right;">Price</th>
+                                <th style="width:25%;">Product</th>
+                                <th style="width:25%;text-align:right;">Price</th>
                                 <th style="width:25%;text-align:center;">Availability</th>
                                 <th style="width:25%;text-align:center;">Action</th>
                             </tr>
@@ -1140,10 +1140,10 @@ export default function POSTransactions() {
                     <table class="cart-items-table" id="cartTable">
                         <thead>
                             <tr>
-                                <th>Item</th>
-                                <th style="text-align:center;">Qty</th>
-                                <th style="text-align:right;">Total</th>
-                                <th style="text-align:center;">Action</th>
+                                <th style="width:25%;">Item</th>
+                                <th style="width:25%;text-align:center;">Qty</th>
+                                <th style="width:25%;text-align:right;">Total</th>
+                                <th style="width:25%;text-align:center;">Action</th>
                             </tr>
                         </thead>
                         <tbody id="cartBody"></tbody>
